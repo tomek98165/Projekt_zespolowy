221a659d2c58ac6846c127efacacecbbe669f47b
Tomek <tomek122145@gmail.com>
2024-04-27 15:29:41 +0200

    Created movie details page

--- frontend/src/MovieDetails.js
+++ frontend/src/MovieDetails.js
@@ -1,12 +1,31 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const MovieDetails = () => {
-    const MovieDetails = () => {
         const {id} = useParams();
-    }
+        const { data: movie, error, isPending } = useFetch("http://localhost:8080/movies/" + id);
+        const history = useHistory();
+
+        const handleClickDelete= () => {
+            fetch('http://localhost:8080/movies/' + id,{
+                method: 'DELETE'
+            }).then(() => {
+                history.push("/")
+            }) 
+        }
     return (
         <div className="movie-details">
-            <h1>Szczególy filmu</h1>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div>}
+            {movie && (
+                <article>
+                    <h2>{ movie.title }</h2>
+                    <p>Wyreżyserował: { movie.director }</p>
+                    <div>{ movie.description }</div>
+                    <button class="delete-button"onClick={handleClickDelete}>Delete</button>
+                </article>
+            )}
         </div>
     );
 }
